3b0b5ea010c6dfdddfc940701ca5025f5c369e76
jacobszpz <jacobszpz@protonmail.com>
2023-03-09 18:15:47 +0000

    Initial commit after basic Spring Boot scaffolding

---
+++ src/test/java/ch/jsan/moderatejokesapi/ModerateJokesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.jsan.moderatejokesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModerateJokesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
